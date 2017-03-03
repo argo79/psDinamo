61e93841ada2661e21e446e8a06b453d8f0adb33
root <melloni6@gmail.com>
2017-03-03 17:33:59 +0100

    Secondo aggiornamento

--- psLazio.R
+++ psLazio.R
@@ -1,29 +1,13 @@
-library(rvest)
-library(htmltab)
-
-ateam <- read_html("https://www.regione.lazio.it/accessiprontosoccorso/")
-tabella<-html_node(ateam, xpath='//*[@id="tab_right"]/table')
-html_table(tabella, fill = TRUE)
-##################################################################################
-View(ateam)
-google_form("1M9B8DsYNFyDjpwSK6ur_bZf8Rv_04ma3rmaaBiveoUI")
-google2 <- read_html(httr::GET("http://google.com"))
-box_office <- read_html("http://www.boxofficemojo.com/movies/?id=ateam.htm")
-box_office %>% html_node("form") %>% html_form()
-
-##################################################################################
-# FUNZIONANTE:
-
 library(htmltab)
 url <- "https://www.regione.lazio.it/accessiprontosoccorso/"
 # ritardo in secondi
-delay<-300
+delay<-5
 # contatore rilevazioni
-contatore<-as.numeric(c(1:6))
+contatore<-as.numeric(c(1:4))
 # Ripetizioni del ciclo di durata1C
-ripetizioni<-as.numeric(c(1:1))
+ripetizioni<-as.numeric(c(1:3))
 # Durata ciclo
-durata1C<-(length(contatore)*delay)/60
+durata1C<-(length(contatore)*delay)/3600
 ciclo<-paste("Durata un ciclo: ", durata1C," ore.", sep="")
 print(ciclo)
 durataRC<-(durata1C*length(ripetizioni))
@@ -32,7 +16,7 @@ print(ripTot)
 # Ora di partenza
 ora<-0
 for (r in ripetizioni) {
-   if (r<=length(ripetizoni)) {
+   if (r<=length(ripetizioni)) {
       for (i in contatore) {
          if (i<=length(contatore)) {
             nome<-paste("pslazio",i,".csv",sep="")
@@ -53,12 +37,25 @@ for (r in ripetizioni) {
          }
       }
       psLazioBak<-psLazioT
+      psLazioT<-data.frame(psLazioT)
+      psLazioT[, c(7:12,14:19,21,23:27,29)] <- sapply(psLazioT[, c(7:12,14:19,21,23:27,29)], as.numeric)
+      colnames(psLazioT)<-c("Struttura","Comune","Asl","Tipo","Aggiornamento","k6","RossoAt","GialloAt","VerdeAt","BiancoAt","NonAt","TotAt","k13","RossoTr","GialloTr","VerdeTr","BiancoTr","NonTr","TotTr","k20","AttesaRic","k22","RossoOs","GialloOs","VerdeOs","BiancoOs","TotOs","k28","TotTot")
+      psLazioT3<-psLazioT[,c(-6,-13,-20,-22,-28)]
+      psLazioT4<-psLazioT3[rowSums(is.na(psLazioT3))==0,]
+      write.table(psLazioT4, file="psLazioTot1.csv", sep=",")
+      attach(psLazioT4)
+      aggdata <-aggregate(psLazioT4, by=list(Struttura), 
+                          FUN=mean, na.rm=TRUE)
+      #print(aggdata)
+      detach(psLazioT4)
       ora<-ora+0.5
+      aggdata$Aggiornamento<-ora
       nomeFT<-paste("psLazioT",ora,".csv",sep="")
-      write.table(psLazioT, file=nomeFT, sep=",")
+      write.table(aggdata, file=nomeFT, sep=",")
    }
 }
 ora<-0
+r<-0
 for (r in ripetizioni) {
    if (r==1) {
       ora<-ora+0.5
@@ -66,37 +63,12 @@ for (r in ripetizioni) {
       psLazioFT<-read.csv(nomeFT)
    }   
    else if (i<length(ripetizioni) || r!=1) {
+      ora<-ora+0.5
       nomeFT<-paste("psLazioT",ora,".csv",sep="")
       psLazioT<-read.csv(nomeFT)
       psLazioFT<-rbind(psLazioFT,psLazioT)
    }
 }
 
-psLazioT<-data.frame(psLazioFT)
-psLazioT[, c(7:12,14:19,21,23:27,29)] <- sapply(psLazioT[, c(7:12,14:19,21,23:27,29)], as.numeric)
-colnames(psLazioT)<-c("Struttura","Comune","Asl","Tipo","Aggiornamento","k6","RossoAt","GialloAt","VerdeAt","BiancoAt","NonAt","TotAt","k13","RossoTr","GialloTr","VerdeTr","BiancoTr","NonTr","TotTr","k20","AttesaRic","k22","RossoOs","GialloOs","VerdeOs","BiancoOs","TotOs","k28","TotTot")
-psLazioT3<-psLazioT[,c(-6,-13,-20,-22,-28)]
-psLazioT4<-psLazioT3[rowSums(is.na(psLazioT3))==0,]
-write.table(psLazioT4, file="psLazioTot.csv", sep=",")
-
-attach(psLazioT4)
-aggdata <-aggregate(psLazioT4, by=list(Struttura), 
-                    FUN=mean, na.rm=TRUE)
-print(aggdata)
-detach(psLazioT4)
-
-################################################################################################
-
-
-psLazioDF2<-data.frame(ID=psLazioT4[,1], Means=rowMeans(psLazioT4[,7]))
-
-#pslazioTTT$VerdeAt<-as.numeric(as.character(pslazioT$VerdeAt))
-#pslazioTT<-transform(pslazioT, pslazioT[,[7:12,14:19,21,23:27,29]]<- as.numeric(pslazioT[,[7:12,14:19,21,23:27,29]]))
-psLazioG<-group_by(psLazioT4, Struttura)
-vettoreColori<-c("RossoAt","GialloAt","VerdeAt","BiancoAt","NonAt","TotAt","RossoTr","GialloTr","VerdeTr","BiancoTr","NonTr","TotTr","AttesaRic","RossoOs","GialloOs","VerdeOs","BiancoOs","TotOs","TotTot")
-i<-'GialloAt'
-for (i in vettoreColori) {
-   psLazioM<-mean(i)
-   psLazioM<-summarise(psLazioG, mean(i))
-}
-psLazioM<-summarise(psLazioG, mean(VerdeAt))
+psLazioBak<-psLazioFT
+write.table(psLazioFT,file="psLazioTot1.csv",sep=",")
